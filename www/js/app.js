03a9a2e3d43192c44d54353dcceec31dab6cb398
Miguel Angel Torres Renteria (Vendor) <vn0x53q@mx.wal-mart.com>
2017-10-03 10:32:59 -0500

    How to use components of ionic.

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+var app = angular.module('starter', ['ionic'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -23,4 +23,39 @@ angular.module('starter', ['ionic'])
       StatusBar.styleDefault();
     }
   });
+});
+
+
+app.controller("MainCtrl", function ($scope) {
+    $scope.email = "type your email.";
+    $scope.password = "type your password."
+    $scope.ratingArr = [{
+        value: 1,
+        icon: 'ion-ios-star-outline'
+    }, {
+        value: 2,
+        icon: 'ion-ios-star-outline'
+    }, {
+        value: 3,
+        icon: 'ion-ios-star-outline'
+    }, {
+        value: 4,
+        icon: 'ion-ios-star-outline'
+    }, {
+        value: 5,
+        icon: 'ion-ios-star-outline'
+    }];
+
+    $scope.setRating = function (val) {
+        var rtgs = $scope.ratingArr;
+        for (var i = 0; i < rtgs.length; i++) {
+            if (i < val) {
+                rtgs[i].icon = 'ion-ios-star';
+            } else {
+                rtgs[i].icon = 'ion-ios-star-outline';
+            }
+        };
+    }
+
+
 });
